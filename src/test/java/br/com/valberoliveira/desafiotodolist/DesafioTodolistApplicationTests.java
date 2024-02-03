32c3ac8309c5587b53fd0820e360530499db20d1
Valber Oliveira <valberoliveira789@gmail.com>
2024-02-03 11:11:18 -0400

    initial commit

---
+++ src/test/java/br/com/valberoliveira/desafiotodolist/DesafioTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.valberoliveira.desafiotodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
